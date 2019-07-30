d96c6ccb4e8150816378bbaf7086328905b12578
Andrew Bancroft <andrewcbancroft@gmail.com>
2019-07-30 12:35:12 -0500

    Initial Commit

---
+++ BlogIdeaList-SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BlogIdeaList-SwiftUI
+//
+//  Created by Andrew Bancroft on 7/30/19.
+//  Copyright © 2019 Andrew Bancroft. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
